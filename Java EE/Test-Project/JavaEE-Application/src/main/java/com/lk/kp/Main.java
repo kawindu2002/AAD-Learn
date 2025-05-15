1c2a70dea6683092358b8342d3fb55f2898e1ab2
kawindu2002 <kawindupriyashan@gmail.com>
2025-05-15 11:51:08 +0530

    add servlet

--- Java EE/Test-Project/JavaEE-Application/src/main/java/com/lk/kp/Main.java
+++ Java EE/Test-Project/JavaEE-Application/src/main/java/com/lk/kp/Main.java
@@ -1,6 +1,18 @@
 package com.lk.kp;
 
-public class Main {
-//     Web app doesn't have main method
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/hello")
+public class Main extends HttpServlet {
+     @Override
+     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+          resp.getWriter().println("Get Method Invoked");
+     }
 }
 
