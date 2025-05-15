e5d02773d9602608061ebbd8ddbdb160e1f486c3
kawindu2002 <kawindupriyashan@gmail.com>
2025-05-15 19:01:10 +0530

    add customer servlet

--- Java EE/Test-Project/JavaEE-Application/src/main/java/com/lk/kp/MainServlet.java
+++ Java EE/Test-Project/JavaEE-Application/src/main/java/com/lk/kp/MainServlet.java
@@ -1,6 +1,5 @@
 package com.lk.kp;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,13 +9,21 @@ import java.io.IOException;
 
 // This servlet will respond to requests sent to the URL "/hello".
 @WebServlet("/hello")
-// We are creating a servlet class named 'Main' that inherits from 'HttpServlet'.
-public class Main extends HttpServlet {
+// We are creating a servlet class named 'Servlet' that inherits from 'HttpServlet'.
+public class MainServlet extends HttpServlet {
      // We are overriding the doGet() method to handle GET requests.
      @Override
      protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-          resp.getWriter().println("Get Method Invoked");
+          //System.out.println(req.getHeader("Age"));
+          
+          // We can change the content type
+          //resp.setContentType("application/json");
+          
+          resp.setHeader("Name","Kawindu");
+          
           // We send a simple text response saying "Get Method Invoked" to the client.
+          resp.getWriter().println("Get Method Invoked");
+          
      }
 }
 
