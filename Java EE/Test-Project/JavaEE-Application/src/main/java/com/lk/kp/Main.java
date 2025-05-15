7a687a843230b2b154c4c104706cac1254a8389c
kawindu2002 <kawindupriyashan@gmail.com>
2025-05-15 13:25:34 +0530

    add comments

--- Java EE/Test-Project/JavaEE-Application/src/main/java/com/lk/kp/Main.java
+++ Java EE/Test-Project/JavaEE-Application/src/main/java/com/lk/kp/Main.java
@@ -8,11 +8,15 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
+// This servlet will respond to requests sent to the URL "/hello".
 @WebServlet("/hello")
+// We are creating a servlet class named 'Main' that inherits from 'HttpServlet'.
 public class Main extends HttpServlet {
+     // We are overriding the doGet() method to handle GET requests.
      @Override
-     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
           resp.getWriter().println("Get Method Invoked");
+          // We send a simple text response saying "Get Method Invoked" to the client.
      }
 }
 
